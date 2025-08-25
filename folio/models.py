944baafd4874df9378223220ecb1c96c1ebab562
idrice junior <dongmojunior18@gmail.com>
2025-08-25 18:07:40 -0500

    model

--- folio/models.py
+++ folio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=123)
+    email = models.EmailField()
+    text = models.TextField(max_length=320)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
