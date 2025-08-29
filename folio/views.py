cdaffb11d24e79c133fc18003ab3e0f8327106fa
idrice junior <dongmojunior18@gmail.com>
2025-08-29 05:53:28 -0500

    done

--- folio/views.py
+++ folio/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 # Create your views here.
 
@@ -9,10 +12,21 @@ def index(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         text = request.POST.get('text')
-
+        message = f"From: {name}\nEmail: {email}\n\nMessage:\n{text}"
         form = Contact(name=name, email=email, text=text)
         form.save()
-        messages.success(request, "message send")
+
+        send_mail(
+            subject=f"New Contact Form Submission from {name}",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=['idrice@gmail.com'],  
+        )
+        return redirect('index') 
+
+
+       
+       
     return render(request, 'folio/index.html')
 def layout(request):
     return render(request,'folio/layout.html')
--- personal/settings.py
+++ personal/settings.py
@@ -125,3 +125,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+## TO SEND THE EMAIL SECTION 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'idricejunior16@gmail.com'
+EMAIL_HOST_PASSWORD = 'pijt uiky pfme wxqh'
